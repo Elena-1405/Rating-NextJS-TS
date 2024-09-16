1260174cd1594ebaab7d93233bcc7b3f8ebc70f0
Elena-1405 <helenofspb@gmail.com>
2024-09-16 20:31:04 +0300

    Sidebar fixing

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -1,13 +1,15 @@
 import { Menu } from '../Menu/Menu';
 import { SidebarProps } from './Sidebar.props';
-//import styles from './Sidebar.module.css';
-//import cn from 'classnames';
+import Logo from '../logo.svg';
+import styles from './Sidebar.module.css';
+import cn from 'classnames';
 
-export const Sidebar = ({ ...props}: SidebarProps): JSX.Element => {
+export const Sidebar = ({ className, ...props}: SidebarProps): JSX.Element => {
     return (
-        <div {...props}>
-           <Menu />
-        </div>
+        <div className={cn(className, styles.sidebar)} {...props}>
+			<Logo className={styles.logo} />
+			<Menu />
+		</div>
 
     );      
 };
