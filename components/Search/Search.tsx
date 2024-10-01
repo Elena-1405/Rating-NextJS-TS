ac06d6bf5361f3490a717df52f428a0b19da90e8
Elena-1405 <helenofspb@gmail.com>
2024-10-01 19:39:54 +0300

    Search component created

---
+++ components/Search/Search.tsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import { Button } from '../Button/Button';
+import { Input } from '../Input/Input';
+import { SearchProps } from './Search.props';
+import GlassIcon from './glass.svg';
+import styles from './Search.module.css';
+import cn from 'classnames';
+import router, { useRouter } from 'next/router';
+
+export const Search = ({ className, ...props }: SearchProps): JSX.Element => {
+    const [search, setSearch] = useState<string>('');
+    const router = useRouter();
+
+    const goToSearch = () => {
+        router.push({
+            pathname: '/search',
+            query: {
+                q: search
+            }
+        });
+    }
+    return (
+        <form className={cn(className, styles.search)} {...props} role='search'>
+            <Input 
+                className={styles.input}
+                placeholder='Поиск'
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+            />
+            <Button 
+                appearance='primary'
+                className={styles.button}//не видит стили в css. To solve
+                onClick={goToSearch}
+                style={{position: 'absolute', //temporary
+                    top: '3px',
+                    right: '3px',
+                    width: '30px',
+                    height: '30px',
+                    padding: '7px'}}
+            >
+                <GlassIcon />
+            </Button>
+        </form>
+    );      
+};
