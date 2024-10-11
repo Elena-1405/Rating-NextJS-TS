3e285286488c88c1225b88855ea513e0330e4325
Elena-1405 <helenofspb@gmail.com>
2024-10-11 16:53:44 +0300

    button component minor fixes

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -6,7 +6,7 @@ import cn from 'classnames';
 export const Button = ({ appearance, arrow ='none', children, className, ...props}: ButtonProps): JSX.Element => {
     return (
         <button
-        className={cn(styles.button, {
+        className={cn(styles.button, className, {
             [styles.primary]: appearance == 'primary',
             [styles.ghost]: appearance == 'ghost',
         })}
