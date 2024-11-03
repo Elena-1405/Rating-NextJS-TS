ed028e57c055a2b80c480542e951b6e47c423157
Elena-1405 <helenofspb@gmail.com>
2024-11-03 14:55:16 +0300

    ReviewForm component created

---
+++ components/ReviewForm/ReviewForm.props.ts
@@ -0,0 +1,5 @@
+import { DetailedHTMLProps, HTMLAttributes } from 'react';
+
+export interface ReviewFormProps extends DetailedHTMLProps<HTMLAttributes<HTMLParagraphElement>, HTMLParagraphElement> {
+    productId: string;
+}
