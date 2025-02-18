edbc30d2a3e1dbe1b249049f1e0d36b93a5b62fb
Elena-1405 <helenofspb@gmail.com>
2025-02-18 18:19:39 +0300

    Aria-hidden and landmarks added

--- layout/Menu/Menu.tsx
+++ layout/Menu/Menu.tsx
@@ -120,8 +120,8 @@ export const Menu = (): JSX.Element => {
     };
 
     return (
-        <div className={styles.menu}>
+        <nav className={styles.menu} role='navigation'>
             {buildFirstLevel()}       
-        </div>
+        </nav>
     );      
 };
