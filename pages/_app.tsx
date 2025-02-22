1d59915717e104f2adee5bf87b1dc3ee30e3a32d
Elena-1405 <helenofspb@gmail.com>
2025-02-22 09:42:22 +0300

    Meta tags added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import Head from 'next/head';
 import '../styles/global.css';
 
-const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
+const MyApp = ({ Component, pageProps, router }: AppProps): JSX.Element => {
   return (
     <>
       <Head>
@@ -12,6 +12,8 @@ const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100..900&display=swap" rel="stylesheet" />
+        <meta name='og:url' content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath} />
+        <meta name='og:locale' content='ru_RU' />
       </Head>
       <Component {...pageProps} />
     </>
